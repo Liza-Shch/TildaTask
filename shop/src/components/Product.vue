23c3ce775cee063ffb3be1c54b5ba8fed0040450
Liza-Shch <scherbakova.liz@yandex.ru>
2019-08-28 11:06:29 +0300

    [T1] Init App + some architecture

---
+++ shop/src/components/Product.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class='product'>
+        <button type='button' class='product__button' @click="addToBusket">Купить</button>
+    </div>
+</template>
+
+<script>
+import EventBus from '@/eventbus.js';
+
+export default {
+    name: 'Product',
+    data() {
+        return {
+            product : {
+                id: 1,
+                name: "jeans"
+            },
+        }
+    },
+    methods: {
+        addToBusket() {
+            EventBus.$emit('add-product-to-basket', this.product);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.product {
+    display: flex;
+    flex-direction: column;
+
+    &__button {
+        width: 300px;
+        height: 50px;
+        background-color: black;
+    }
+}
+</style>
+
+
---
+++ shop/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ShopHeader mainTitle="Juicy" subTitle='Не бойся быть ярким!'/>
+    <Product />
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ShopHeader from '@/components/Header.vue';
+import Product from '@/components/Product.vue';
+
+export default {
+  name: 'home',
+  components: {
+    ShopHeader,
+    Product
+  },
+};
+</script>
